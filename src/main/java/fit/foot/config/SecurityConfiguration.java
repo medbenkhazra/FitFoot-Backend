b10f88884a8a72dabe3cdee56161c82f65f2b31c
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-12-11 20:14:25 +0100

    complexe excempt

--- src/main/java/fit/foot/config/SecurityConfiguration.java
+++ src/main/java/fit/foot/config/SecurityConfiguration.java
@@ -103,6 +103,7 @@ public class SecurityConfiguration {
             .pathMatchers("/api/account/reset-password/init").permitAll()
             .pathMatchers("/api/account/reset-password/finish").permitAll()
                 .pathMatchers("/api/reservations/**").permitAll()
+                .pathMatchers("/api/complexes/**").permitAll()
             .pathMatchers("/api/terrains/**").permitAll()
         .pathMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .pathMatchers("/api/**").authenticated()
