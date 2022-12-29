c16e77b0b178d54711eaec39ed87146f19f204ba
MOHAMED <mohammed.benkhazra@edu.uca.ma>
2022-12-29 01:22:39 +0100

    fix-1

--- terraform/modules/mysql/main.tf
+++ terraform/modules/mysql/main.tf
@@ -20,7 +20,7 @@ resource "random_password" "password" {
 }
 
 resource "azurerm_mysql_flexible_server" "database" {
-  name                = azurecaf-name.mysql-server.result
+  name                = fitfootdb
   resource_group_name = var.resource_group
   location            = var.location
 
