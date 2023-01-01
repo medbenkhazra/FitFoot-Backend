ff80bc193817b1c76988a4791e3fd036b53b7d98
MOHAMED <mohammed.benkhazra@edu.uca.ma>
2023-01-01 17:29:22 +0100

    add resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurecaf_name" "resource_group" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "rg-terraform-001"
+  name     = "rg-terraform-002"
   location = var.location
 
   tags = {
